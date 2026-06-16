0e85f65196cbc61580ca3bbf54583eab3f55b409
astibagladstone-hue <astibagladstone@gmail.com>
2026-06-16 16:37:51 +0300

    Fix dashboard namespace and remove login

--- pssf_dashboard/urls.py
+++ pssf_dashboard/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from dashboard import views
 
 urlpatterns = [
 
     path("admin/", admin.site.urls),
 
-    # Dashboard without login
-    path("", views.home, name="home"),
-
-    # Dashboard namespace
     path("", include("dashboard.urls")),
+
 ]
